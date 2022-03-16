ab6cdf069306f08a627011913e7c6aa3e9bb8001
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-15 23:25:02 -0300

    adicionando campo select

--- src/components/Campos/index.tsx
+++ src/components/Campos/index.tsx
@@ -1,6 +1,7 @@
-import { Grid } from "@mui/material";
+import { Grid, MenuItem } from "@mui/material";
 import { Field } from "formik";
-import { TextField } from "formik-mui";
+import { Select, TextField } from "formik-mui";
+import { ReactNode } from "react";
 import styled from "styled-components";
 
 
@@ -16,7 +17,7 @@ interface CampoFormularioProps {
 export function CampoFormulario(props: CampoFormularioProps) {
   return (
     <Grid md={12} xs={12} item>
-      <FieldEstilizado
+      <CampoFormularioEstilizado
         id={props.id}
         label={props.label}
         name={props.name}
@@ -29,6 +30,37 @@ export function CampoFormulario(props: CampoFormularioProps) {
   );
 }
 
-const FieldEstilizado = styled(Field)`
+const CampoFormularioEstilizado = styled(Field)`
   width: 100%;
 `;
+
+interface CampoSelcectFormularioProps {
+  data: {
+    valueMenuItem: string | number | readonly string[];
+    chidrenMenuItem: ReactNode;
+  }[];
+  id: string;
+  label: string;
+  labelId: string;
+  name: string;
+}
+
+export function CampoSelcectFormulario(props: CampoSelcectFormularioProps) {
+  return (
+    <Field
+      component={Select}
+      id={props.id}
+      name={props.name}
+      labelId={props.labelId}
+      label={props.label}
+    >
+      {props.data.map((item, index) => {
+        return (
+          <MenuItem key={index} value={item.valueMenuItem}>
+            {item.chidrenMenuItem}
+          </MenuItem>
+        );
+      })}
+    </Field>
+  );
+}
