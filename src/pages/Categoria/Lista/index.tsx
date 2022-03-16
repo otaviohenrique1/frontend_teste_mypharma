910d50f0b9e40b61eb826a053165842bcfb8392a
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-15 21:33:32 -0300

    Criando modulo de categoria

---
+++ src/pages/Categoria/Lista/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Grid } from "@mui/material";
+
+export function ListaProduto() {
+  return (
+    <Container maxWidth="lg">
+      <Grid md={12}>
+        <h1>Lista de produtos</h1>
+      </Grid>
+    </Container>
+  );
+}
