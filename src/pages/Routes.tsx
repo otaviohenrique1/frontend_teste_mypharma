aad3f04060f7f7fa193287a793b8a8baffc4f43e
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-16 21:22:38 -0300

    adicionando rotas:  ListaCategorias  ListaMarcas  Pagina404

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CadastroCategoria } from "./Categoria/Cadastro";
+import { ListaCategorias } from "./Categoria/Lista";
 import { HomePage } from "./HomePage";
 import { Login } from "./Login";
 import { CadastroMarca } from "./Marca/Cadastro";
+import { ListaMarcas } from "./Marca/Lista";
+import { Pagina404 } from "./Pagina404";
 import { CadastroProduto } from "./Produto/Cadastro";
 import { EdicaoProduto } from "./Produto/Edicao";
 import { ListaProduto } from "./Produto/Lista";
@@ -18,16 +21,17 @@ export function RoutesApp() {
         <Route path="/produtos/cadastro" element={<CadastroProduto />} />
         <Route path="/produtos/:id/edicao" element={<EdicaoProduto />} />
 
-        <Route path="/marcas" element={<ListaProduto />} />
+        <Route path="/marcas" element={<ListaMarcas />} />
         <Route path="/marcas/cadastro" element={<CadastroMarca />} />
         <Route path="/marcas/:id/edicao" element={<EdicaoProduto />} />
 
-        <Route path="/categorias" element={<ListaProduto />} />
+        <Route path="/categorias" element={<ListaCategorias />} />
         <Route path="/categorias/cadastro" element={<CadastroCategoria />} />
         <Route path="/categorias/:id/edicao" element={<EdicaoProduto />} />
 
         <Route path="/usuarios" element={<ListaProduto />} />
         <Route path="/usuarios/cadastro" element={<CadastroUsuario />} />
+        <Route element={<Pagina404 />} path="*" />
       </Routes>
     </BrowserRouter>
   );
